49c91c4790a0b12a05fe08e3d229e025f3ca982b
rumians <ruanfort12@gmail.com>
2023-11-16 09:29:30 -0300

    idiomas

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
 
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
 
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
-});
-
-valor2.addEventListener("keyup", function() {
+let btSoma = document.getElementById("btSomar");
+btSoma.addEventListener("click" , function () {
     somar(Number(valor1.value), Number(valor2.value));
 });
 
 function somar(a, b) {
-    let soma = a + b;
-    resultado.innerHTML = soma;
+    resultado.textContent = a + b;
 }
+
+let btSubtrair = document.getElementById("btSubtrair");
+btSubtrair.addEventListener("click" , function () {
+    sub(Number(valor1.value), Number(valor2.value));
+});
+
+function sub(a, b) {
+    resultado.textContent = a - b;
+}
+
+let btMultiplicar = document.getElementById("btMultiplicar");
+btMultiplicar.addEventListener("click" , function () {
+    mult(Number(valor1.value), Number(valor2.value));
+});
+
+function mult(a, b) {
+    resultado.textContent = a * b;
+}
+
+let btDividir = document.getElementById("btDividir");
+btDividir.addEventListener("click" , function () {
+    div(Number(valor1.value), Number(valor2.value));
+});
+
+function div(a, b) {
+    resultado.textContent = a / b;
+}
+
+let idioma = document.getElementById("idioma")
+idioma.addEventListener("change", function () {
+        if (this.value == "br") {
+            btSoma.value = "Somar";
+        } else if (this.value == "es") {
+            btSoma.value = "Sumar";
+        }
+    })
